bac8bf66b961f9044216adf0ba67d05041d6c1a8
halfmoon01 <onurivit01@gmail.com>
2024-11-07 23:30:00 +0900

    commit6: build user information page (dialog)

--- assignment3/src/pages/MyInfo.jsx
+++ assignment3/src/pages/MyInfo.jsx
@@ -5,7 +5,15 @@
 import React, { useState } from 'react';
 import userIcon from '../AssignImages/user.png';
 import './MyInfo.css'; 
+import ChangePwd from '../dialog/ChangePwd'
+import ChangeImage from '../dialog/ChangeImage'
+import ChangeName from '../dialog/ChangeName'
+
+
 const UserInfo = () => {
+  const [isImageOpen, setIsImageOpen] = useState(false);
+  const [isPwdOpen, setIsPwdOpen] = useState(false);
+  const [isNameOpen, setIsNameOpen] = useState(false);
   return (
     <div className="myInfo">
       <h1>User Information</h1>
@@ -14,22 +22,30 @@ const UserInfo = () => {
           <img src={userIcon} width="150" height="150" alt="User" />
         </div>
       </figure>
-      <button className="button_type1" type="button" onClick={() => setIsImagePopupOpen(true)}>
+      <button className="button_type1" type="button" onClick={() => setIsImageOpen(true)}>
         Change Image
       </button>
       <p>Email: Sanghyun.Jun.1@stonybrook.edu</p>
       <p>
         Password: ****** <br/><br/>
-        <button className="button_type1" type="button" onClick={() => setIsPwdPopupOpen(true)}>
+        <button className="button_type1" type="button" onClick={() => setIsPwdOpen(true)}>
           Change Password
         </button>
       </p>
       <p>
         Name: Sanghyun Jun <br/><br/>
-        <button className="button_type1" type="button" onClick={() => setIsNamePopupOpen(true)}>
+        <button className="button_type1" type="button" onClick={() => setIsNameOpen(true)}>
           Change Name
         </button>
       </p>
+
+      <ChangeImage isOpen={isImageOpen} onClose=
+        {() => setIsImageOpen(false)} />
+      <ChangePwd isOpen={isPwdOpen} onClose=
+        {() => setIsPwdOpen(false)} />
+      <ChangeName isOpen={isNameOpen} onClose=
+        {() => setIsNameOpen(false)} />
+        
     </div>
   );
 };
